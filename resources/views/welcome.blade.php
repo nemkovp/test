af233ee316ac530f1c087928f160168dd732cd03
nemkovp@list.ru <nemkovp@list.ru>
2020-03-31 10:54:23 +0300

    fff

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+@extends('app')
+@section('content')
+  <div class="row">
+     <div class="col-3 mt-3">
+        <div class="card" style="width: 18rem;">
+           <img src="http://orion42.net/wp-content/uploads/2019/05/git-logo.png" class="card-img-top" alt="...">
+           <div class="card-body">
+              <h5 class="card-title">Название карточки</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+           </div>
+        </div>
+     </div>
+     <div class="col-3 mt-3">
+        <div class="card" style="width: 18rem;">
+           <img src="http://orion42.net/wp-content/uploads/2019/05/git-logo.png" class="card-img-top" alt="...">
+           <div class="card-body">
+              <h5 class="card-title">Название карточки</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+           </div>
+        </div>
+     </div>
+     <div class="col-3 mt-3">
+        <div class="card" style="width: 18rem;">
+           <img src="http://orion42.net/wp-content/uploads/2019/05/git-logo.png" class="card-img-top" alt="...">
+           <div class="card-body">
+              <h5 class="card-title">Название карточки</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+           </div>
+        </div>
+     </div>
+     <div class="col-3 mt-3">
+        <div class="card" style="width: 18rem;">
+           <img src="http://orion42.net/wp-content/uploads/2019/05/git-logo.png" class="card-img-top" alt="...">
+           <div class="card-body">
+              <h5 class="card-title">Название карточки</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+           </div>
+        </div>
+     </div>
+     <div class="col-3 mt-3">
+        <div class="card" style="width: 18rem;">
+           <img src="http://orion42.net/wp-content/uploads/2019/05/git-logo.png" class="card-img-top" alt="...">
+           <div class="card-body">
+              <h5 class="card-title">Название карточки</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+           </div>
+        </div>
+     </div>
+  </div>
+@endsection
